ddeab00ae3e6354afecc04f9a4db25401801a6bf
Ayan <44862892+ayan8125@users.noreply.github.com>
2020-07-24 14:13:40 +0530

    Update settings.py

--- challenge2/whatsbussy/settings.py
+++ challenge2/whatsbussy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '48as(&ghwpmyj^lynal3#t$dvaj4ky4!p*l@jaiw0*s=-b@_sd'
+SECRET_KEY = '*********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
